420fa3fc34125e038491e503f9084ac0d4ade298
Denys Fokashchuk <123denysfok123@gmail.com>
2021-07-15 10:20:38 +0300

    first commit

---
+++ ConsoleApp.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+    string name;
+    Person()
+    {
+        name = "";
+    }
+    Person(string name)
+    {
+        this->name = name;
+
+    }
+    static void validate_name(string name)
+    {
+        if (name.empty())
+        {
+            throw "Name can't be empty!";
+        }
+    }
+
+};
+
+int main()
+{
+    Person *pers = new Person;
+    {
+        Person* pers2 = pers;
+    }
+    //Person::validate_name(*pers.name);
+    std::cout << "Hello World!\n";
+
+}
+
